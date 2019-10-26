94c45b9fbbaed4f40c3623777759f1f4b570c8de
ariltw <ariltw@live.com>
2019-10-26 14:41:53 +0700

    method injection

--- app/src/main/java/dev/ariltw/dagger/Car.kt
+++ app/src/main/java/dev/ariltw/dagger/Car.kt
@@ -2,11 +2,16 @@ package dev.ariltw.dagger
 
 import javax.inject.Inject
 
-class Car @Inject constructor(engine: Engine, wheels: Wheels) {
-    lateinit var engine: Engine
+class Car @Inject constructor(wheels: Wheels) {
+    @Inject lateinit var engine: Engine
     lateinit var wheels: Wheels
 
     fun drive(): Unit {
 
+    }
+
+    @Inject
+    fun enableRemote(remote: Remote) {
+        remote.setListener(this)
     }
 }
---
+++ app/src/main/java/dev/ariltw/dagger/Remote.kt
@@ -0,0 +1,12 @@
+package dev.ariltw.dagger
+
+import java.util.logging.Logger
+import javax.inject.Inject
+
+class Remote @Inject constructor() {
+    val TAG: String = Remote::class.java.simpleName
+
+    fun setListener(car: Car) {
+        Logger.getLogger(TAG).info("Remote on")
+    }
+}
